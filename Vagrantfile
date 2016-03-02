fe953e94db7a01839e429ec795318b6b471561b3
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-03-02 16:33:24 +0100

    HEL-10 - Include RabbitMq: added vagrant for rabbitmq

---
+++ Vagrantfile
@@ -0,0 +1,26 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANT_API = "2"
+
+Vagrant.configure(VAGRANT_API) do |config|
+
+  # Use debian as our box
+  config.vm.box = "ubuntu/trusty64"
+
+  # run this script at initial setup
+  config.vm.provision :shell, :path => "devinfo/provision.sh"
+
+  config.vm.provider "virtualbox" do |v|
+     v.name = "project-helin"
+     v.memory = 3024
+     v.cpus = 4
+  end
+	 
+  # RabbitMQ Default port
+  config.vm.network :forwarded_port, host: 5672, guest: 5672
+
+  # For Management UI
+  config.vm.network :forwarded_port, host: 15672, guest: 15672
+
+end
---
+++ devinfo/provision.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# update before the show begins
+apt-get update
+
+# Some basic tools
+apt-get install -y vim 
+
+# RabbitMQ
+
+# Add RabbitMQ to source list
+echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
+curl http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -
+
+# update APT
+apt-get update
+# Install RabbitMQ
+apt-get install rabbitmq-server -y
+
+# Enable Management Console:
+# Connect to port 15672 and you'll be provided with an UI to manager RabbitMQ
+rabbitmq-plugins enable rabbitmq_management
+
+# Add new user 'admin' with password 'helin'
+sudo rabbitmqctl add_user admin helin
+sudo rabbitmqctl set_user_tags admin administrator
+sudo rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
