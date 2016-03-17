fceaff8cbd16d078f9c80a63e9c10e6184e62748
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-03-17 16:41:17 +0100

    HEL-17 - Setup TeamCity for Server: added postgres to vagrant

--- devinfo/provision.sh
+++ devinfo/provision.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export DB_USER=helin
+
 # update before the show begins
 apt-get update
 
@@ -25,4 +27,22 @@ sudo rabbitmqctl add_user admin helin
 sudo rabbitmqctl set_user_tags admin administrator
 sudo rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
 
-# TODO: Install PostgreSQL
+# Install PostgreSQL
+sudo apt-get install -y postgresql postgresql-contrib
+
+# create new user and DB
+sudo -u postgres createdb $DB_USER 
+sudo -u postgres createuser $DB_USER -s       # -s for superuser
+# change password
+sudo -u postgres psql -c "alter user $DB_USER with password '$DB_USER';"
+
+# We need to alter vagrant configuration, so that we can connect from
+# outside of the virtual machine to the db
+# From: http://jamie.ideasasylum.com/2012/09/connecting-navicat-to-postgresql-on-vagrant/
+# Another source: http://www.bentedder.com/use-pgadmin-access-postgres-database-within-vagrant-box/
+
+sudo sh -c  "echo 'host all all all password' >> /etc/postgresql/9.3/main/pg_hba.conf"
+sudo sh -c  "echo listen_addresses = \'*\' >> /etc/postgresql/9.3/main/postgresql.conf"
+
+# restart postgres so that all changes get activated
+sudo /etc/init.d/postgresql restart
