7a8246d7af47690db9aab81aa54cc7939c1d4dfa
Marcel Amsler <marcel.amsler@gmail.com>
2016-03-31 15:44:19 +0200

    worked on interactions with polygons

--- app/assets/javascripts/projects/zoneEditor/zone-map.directive.js
+++ app/assets/javascripts/projects/zoneEditor/zone-map.directive.js
@@ -11,69 +11,106 @@
             template: '<div id="map" class="map"></div>',
             link: function (scope) {
 
+                scope.$watch('selectedZone', function (newValue, oldValue) {
+                    removeCurrentInteractions();
+
+                    if (newValue && newValue !== oldValue) {
+                        if (newValue.polygon === null) {
+                            scope.map.addInteraction(scope.drawInteraction);
+                        } else {
+                            scope.modifyInteraction = getModifyInteraction(newValue);
+                            scope.map.addInteraction(scope.modifyInteraction);
+                        }
+                    }
+                });
+
+
                 var raster = new ol.layer.Tile({
                     source: new ol.source.OSM()
                 });
 
                 var format = new ol.format.WKT();
 
-                scope.polygons = getPolygonsFromZones(scope.zones);
+                scope.readOnlyFeatures = getFeaturesFromZones(scope.zones);
 
-                var vector = new ol.layer.Vector({
+                var vectorLayer = new ol.layer.Vector({
                     source: new ol.source.Vector({
-                        features: scope.polygons
+                        features: scope.readOnlyFeatures
                     })
                 });
 
                 scope.map = new ol.Map({
                     target: 'map',
-                    layers: [raster, vector],
+                    layers: [raster, vectorLayer],
                     view: new ol.View({
                         center: [981481.3, 5978619.7],
                         zoom: 18
                     })
                 });
 
-                var modify = new ol.interaction.Modify({
-                    features: scope.polygons,
-                    // the SHIFT key must be pressed to delete vertices, so
-                    // that new vertices can be drawn at the same position
-                    // of existing vertices
-                    deleteCondition: function (event) {
-                        return ol.events.condition.shiftKeyOnly(event) &&
-                            ol.events.condition.singleClick(event);
-                    }
+                scope.drawInteraction = new ol.interaction.Draw({
+                    features: scope.readOnlyFeatures,
+                    type: 'Polygon'
                 });
 
-                scope.map.addInteraction(modify);
+                function getModifyInteraction(zone) {
+                    var modifyFeatures = new ol.Collection();
+                    modifyFeatures.push(removeFeatureFromReadonlyCollection(zone));
 
-                function addInteraction() {
-                    var draw = new ol.interaction.Draw({
-                        features: scope.polygons,
-                        type: 'Polygon'
-                    });
-
-                    scope.map.addInteraction(draw);
-                }
-
-                function getPolygonsFromZones(zones) {
-                    var polygons = new ol.Collection();
-
-                    zones.forEach(function (zone) {
-                        if (zone.polygon !== null) {
-                            polygons.push(
-                                format.readFeature(zone.polygon, {
-                                    dataProjection: 'EPSG:4326',
-                                    featureProjection: 'EPSG:3857'
-                                })
-                            );
+                    return new ol.interaction.Modify({
+                        features: modifyFeatures,
+                        // the SHIFT key must be pressed to delete vertices, so
+                        // that new vertices can be drawn at the same position
+                        // of existing vertices
+                        deleteCondition: function (event) {
+                            return ol.events.condition.shiftKeyOnly(event) &&
+                                ol.events.condition.singleClick(event);
                         }
                     });
-
-                    return polygons;
                 }
 
-                addInteraction();
+                function removeCurrentInteractions() {
+                    if (scope.modifyInteraction) {
+                        scope.map.removeInteraction(scope.modifyInteraction);
+                        delete scope.modifyInteraction;
+                    }
+
+                    scope.map.removeInteraction(scope.drawInteraction);
+                }
+
+
+                function getFeaturesFromZones(zones) {
+                    var readOnlyFeatures = new ol.Collection();
+
+                    zones.forEach(function (zone) {
+                            if (zone.polygon !== null) {
+                                readOnlyFeatures.push(getFeatureFromZone(zone));
+                            }
+                        }
+                    );
+
+                    return readOnlyFeatures;
+                }
+
+                function removeFeatureFromReadonlyCollection (zone) {
+                    if (scope.readOnlyFeatures) {
+                        return vectorLayer.getSource().getFeatureById(zone.id);
+                    }
+                }
+
+                function getFeatureFromZone(zone) {
+
+                    var feature = format.readFeature(zone.polygon, {
+                        dataProjection: 'EPSG:4326',
+                        featureProjection: 'EPSG:3857'
+                    });
+
+                    feature.setId(zone.id);
+
+                    return feature;
+
+                }
+
             },
             controller: 'ZoneMapCtrl'
         };
