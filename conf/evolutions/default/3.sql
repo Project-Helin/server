ffcf5765215373c4da88ecb3d8e70e04aef3cc37
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-04-07 22:33:45 +0200

    HEL-28 - CRUD for Zones: changed order

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -1,14 +1,18 @@
 # CREATE ZONE TABLE
 
 # --- !Ups
-CREATE TABLE project
+CREATE TABLE zone
 (
-  id                  UUID                  NOT NULL PRIMARY KEY,
+  id         UUID                    NOT NULL PRIMARY KEY,
 
-  name                VARCHAR(200)          NOT NULL,
-  organisation_id     UUID                  NOT NULL REFERENCES organisation (id)
+  --- 4326 is EPSG-Code for WGS84 projection
+  geom       GEOMETRY(POLYGON, 4326) NOT NULL,
+  height     INTEGER                 NOT NULL,
+  TYPE       VARCHAR(200)            NOT NULL,
+  project_id UUID                    NOT NULL REFERENCES project (id)
 );
+
 
 # --- !Downs
 
-DROP TABLE project;
+DROP TABLE zone;
