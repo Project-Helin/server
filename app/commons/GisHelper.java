19e0c21fd22d338f8a43d887a73003889621d1bf
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-04-07 09:58:19 +0200

    HEL-31 - CRUD for Projects: added helper method to get Point from longitude and latitude

--- app/commons/GisHelper.java
+++ app/commons/GisHelper.java
@@ -9,7 +9,8 @@ import org.geolatte.geom.codec.Wkt;
  * Contains common static methods related to post gis
  */
 public class GisHelper {
-    private GisHelper() {}
+    private GisHelper() {
+    }
 
     /*
      * Converts the point to WKT without the SRID information
@@ -22,4 +23,13 @@ public class GisHelper {
         return wktWith.replace("SRID=4326;", "");
     }
 
+
+    /**
+     * Create point from longitude and latitude using WGS-84
+     * ( see http://spatialreference.org/ref/epsg/wgs-84/ )
+     */
+    public static Point createPoint(long longitude, long latitude) {
+        Geometry<?> geometry = Wkt.fromWkt("SRID=4326; POINT (" + longitude + " " + latitude + ")" );
+        return (Point) geometry;
+    }
 }
--- test/commons/GisHelperTest.java
+++ test/commons/GisHelperTest.java
@@ -1,6 +1,8 @@
 package commons;
 
 import org.geolatte.geom.Geometry;
+import org.geolatte.geom.Point;
+import org.geolatte.geom.Position;
 import org.geolatte.geom.codec.Wkt;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -14,10 +16,17 @@ public class GisHelperTest {
 
     @Test
     public void shouldConvertPointToWkt() {
-        Geometry<?> geometry = Wkt.fromWkt("POINT (30 10)");
+        Geometry<?> geometry = Wkt.fromWkt("SRID=4326; POINT (30 10)");
         logger.info("{}", geometry);
 
         String wkt = GisHelper.toWktStringWithoutSrid(geometry);
         assertThat(wkt).isEqualTo("POINT(30 10)");
+    }
+
+    @Test
+    public void shouldCreatePoint() {
+        Point<?> point = GisHelper.createPoint(100, -150);
+
+        assertThat(point.toString()).isEqualTo("SRID=4326;POINT(100 -150)");
     }
 }
