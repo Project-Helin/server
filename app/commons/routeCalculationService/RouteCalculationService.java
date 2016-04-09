fd4d08e457dced4db06b92e38aff001f88bfcc4c
Styp <mstypinski@gmail.com>
2016-04-09 11:34:35 +0200

    added Outline for RouteCalculationService

---
+++ app/commons/routeCalculationService/RouteCalculationService.java
@@ -0,0 +1,18 @@
+package commons.routeCalculationService;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import models.OrderWaypoint;
+import models.Route;
+import models.Zone;
+
+import java.util.List;
+
+public class RouteCalculationService {
+
+    public Route calculateRoute(List<OrderWaypoint> orderWaypoints, Coordinate startCoordinate, List<Zone> zones){
+       return new Route();
+    }
+
+
+
+}
