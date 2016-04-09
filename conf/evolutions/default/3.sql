18a4e8c0fe9bc4ae70f7ad4d2918850eae633cc8
Styp <mstypinski@gmail.com>
2016-04-09 16:04:02 +0200

    changed geometry type to geography for easier handling...

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -6,7 +6,7 @@ CREATE TABLE zone
   id         UUID          NOT NULL PRIMARY KEY,
 
   --- 4326 is EPSG-Code for WGS84 projection
-  polygon    GEOMETRY(POLYGON, 4326),
+  polygon    GEOGRAPHY(POLYGON, 4326),
   height     INTEGER       NOT NULL,
   TYPE       VARCHAR(200)  NOT NULL,
   name       VARCHAR(2000) NOT NULL,
