bdfda1cbeeca14ca9e4ed7b910be21a251f3ff71
Marcel Amsler <marcel.amsler@gmail.com>
2016-04-14 11:23:17 +0200

    formatted code

--- app/commons/ModelHelper.java
+++ app/commons/ModelHelper.java
@@ -1,18 +1,19 @@
 package commons;
+
 import org.apache.commons.beanutils.BeanUtilsBean;
 
 import java.lang.reflect.InvocationTargetException;
 
-public class ModelHelper extends BeanUtilsBean{
+public class ModelHelper extends BeanUtilsBean {
 
     @Override
     public void copyProperty(Object dest, String name, Object value)
             throws IllegalAccessException, InvocationTargetException {
-        if(value==null)return;
+        if (value == null) return;
         super.copyProperty(dest, name, value);
     }
 
-    public static void updateAttributes (Object destination, Object source) {
+    public static void updateAttributes(Object destination, Object source) {
         try {
             new ModelHelper().copyProperties(destination, source);
         } catch (IllegalAccessException e) {
