3fa894e57daf0c2ffeadf20010290e7a2c78c831
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-04-14 18:06:01 +0200

    upps this is needed again - because table name is different than clss name

--- app/dao/ProductsDao.java
+++ app/dao/ProductsDao.java
@@ -12,4 +12,11 @@ public class ProductsDao extends AbstractDao<Product> {
         super(Product.class);
     }
 
+    @Override
+    public List<Product> findAll() {
+        String sql = "select e from products e ";
+        return jpaApi.em()
+                .createQuery(sql, Product.class)
+                .getResultList();
+    }
 }
