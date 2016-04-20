67330753ba449cdc135758d57f3afa8c21083c5d
Styp <mstypinski@gmail.com>
2016-04-20 13:23:08 +0200

    added RouteCalculationService inital commit

--- app/commons/routeCalculationService/RouteCalculationService.java
+++ app/commons/routeCalculationService/RouteCalculationService.java
@@ -1,19 +1,38 @@
-//package commons.routeCalculationService;
-//
-//import com.vividsolutions.jts.geom.Coordinate;
-//import models.OrderWaypoint;
-//import models.Route;
-//import models.Zone;
-//
-//import java.util.List;
-//
-//public class RouteCalculationService {
-//
-//    public Route calculateRoute(List<OrderWaypoint> orderWaypoints, Coordinate startCoordinate, List<Zone> zones){
-//
-//       return new Route();
-//    }
-//
-//
-//
-//}
+package commons.routeCalculationService;
+
+import ch.helin.messages.dto.way.Route;
+import ch.helin.messages.dto.way.Waypoint;
+import com.vividsolutions.jts.geom.Coordinate;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+
+import commons.dijkstra.Dijkstra;
+import models.Zone;
+import org.geolatte.geom.Polygon;
+import org.geolatte.geom.jts.JTS;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RouteCalculationService {
+
+    public Route calculateRoute(List<Waypoint> waypoints, Waypoint startPoint, List<Zone> zones){
+
+        Zone zone = zones.get(0);
+
+        List<Coordinate> coordinates = PolygonManipulationHelper.degeneratePolygonToPointList(zone.getPolygon());
+
+
+        //coordinates.add()
+        System.out.println(coordinates);
+
+        return new Route();
+    }
+
+
+
+
+
+
+}
--- test/commons/routeCalculationService/RouteCalculationServiceTest.java
+++ test/commons/routeCalculationService/RouteCalculationServiceTest.java
@@ -1,13 +1,67 @@
 package commons.routeCalculationService;
 
+import ch.helin.messages.dto.Action;
+import ch.helin.messages.dto.way.Position;
+import ch.helin.messages.dto.way.Route;
+import ch.helin.messages.dto.way.Waypoint;
+import commons.AbstractIntegrationTest;
+import commons.gis.GisHelper;
+import models.Organisation;
+import models.Zone;
+import org.geolatte.geom.Geometry;
+import org.geolatte.geom.Polygon;
 import org.junit.Test;
+import play.db.jpa.JPA;
+import play.db.jpa.JPAApi;
+
+import javax.inject.Inject;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by martin on 09.04.16.
- */
-public class RouteCalculationServiceTest {
+
+public class RouteCalculationServiceTest extends AbstractIntegrationTest {
+
+    @Test
+    public void initTest(){
+        List<Waypoint> waypointList = new ArrayList<>();
+
+        Waypoint startPoint = new Waypoint();
+        Waypoint dropPoint = new Waypoint();
+
+        startPoint.setAction(Action.TAKEOFF);
+        startPoint.setPosition(new Position(8.8153890, 47.2237834));
+
+        dropPoint.setAction(Action.DROP);
+        startPoint.setPosition(new Position(8.8164874, 47.2235279));
+
+        waypointList.add(startPoint);
+        waypointList.add(dropPoint);
+
+
+        Zone zone = new Zone();
+        org.geolatte.geom.Geometry geometry = GisHelper.convertFromWKBToGeometry("0103000020E6100000010000000F000000FFB" +
+                "E7D4109A2214002A052D59E9C474031D6A58B22A2214091F301CF999C4740DBD8E020E4A121401D871466909C47403720F1E" +
+                "8D2A1214051F0BB048D9C4740305A7EEBC4A12140B1324EBD8B9C474033FF231A9DA1214091AF6CC7939C474076EC7CBC82A" +
+                "12140510DF2439A9C4740ABA84A3574A1214039F1A836A29C4740E98D44AA7AA12140979DDE81A69C47409C77BF0B80A12140" +
+                "74325B54A79C47401FC639C3C9A1214075CB0D85969C474086992F86D4A121403483A03D959C4740248CACC0D7A121408CC91" +
+                "7B5979C4740248CACC0D7A121408CC917B5979C4740FFBE7D4109A2214002A052D59E9C4740");
+
+        zone.setPolygon((Polygon) geometry);
+        zone.setName("testpolygon");
+
+        List<Zone> zoneList = new ArrayList<>();
+        zoneList.add(zone);
+
+
+        RouteCalculationService routeCalculationService = new RouteCalculationService();
+        Route route = routeCalculationService.calculateRoute(waypointList, startPoint, zoneList);
+    }
+
+
+
 
 
 }
