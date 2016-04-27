560c0568d480bffe3e4d85d3b7b796121ee22610
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-04-27 11:09:54 +0200

    HEL-60 - Secure Crud for Project, Product and drone: added comment and made code simpler

--- app/commons/TemplateHelper.java
+++ app/commons/TemplateHelper.java
@@ -3,35 +3,47 @@ package commons;
 import dao.UserDao;
 import models.Organisation;
 import models.User;
+import play.api.Application;
 import play.api.Play;
+import play.api.inject.Injector;
+import play.db.jpa.JPA;
 import play.db.jpa.JPAApi;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
+/**
+ * Since Play does not allow to access the DB from template,
+ * this method provides methods to do so.
+ */
 public class TemplateHelper {
 
+
     public static List<Organisation> getOrganisations(final String userId) {
-        JPAApi jpaApi = Play.application(Play.current()).injector().instanceOf(JPAApi.class);
+        /**
+         * This is a bit ugly, but Play has deprecated the static method JPA.withTransaction()
+         * but it does not provide an alternative to get an instance of JPA.
+         *
+         * Here we use some static methods to get the current application,
+         * get the injector from that application, and get an instance
+         * of JPAApi ( the replacement to JPA )
+         */
+        Application currentApplication = Play.application(Play.current());
+        Injector injector = currentApplication.injector();
 
-        final ArrayList<Organisation> organisations = new ArrayList<>();
+        JPAApi jpaApi = injector.instanceOf(JPAApi.class);
+        UserDao userDao = injector.instanceOf(UserDao.class);
 
-        jpaApi.withTransaction(() -> {
-            UserDao userDao = Play.application(Play.current()).injector().instanceOf(UserDao.class);
+        return jpaApi.withTransaction((em) -> {
             User user = userDao.findById(UUID.fromString(userId));
+
             if (user != null) {
                 Set<Organisation> loadedOrganisations = user.getOrganisations();
-                organisations.addAll(loadedOrganisations);
+                return new ArrayList<>(loadedOrganisations);
+            }else {
+                return Collections.emptyList();
             }
-        });
 
-        if (!organisations.isEmpty()) {
-            return organisations;
-        } else {
-            return new ArrayList<>();
-        }
+        });
     }
 
 
