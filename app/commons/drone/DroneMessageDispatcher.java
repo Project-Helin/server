196000e58ea78e6072d5c3a5c0c5e924836109f7
Marcel Amsler <marcel.amsler@gmail.com>
2016-05-13 15:34:46 +0200

    added missionctrl test and improved testhelper

--- app/commons/drone/DroneMessageDispatcher.java
+++ app/commons/drone/DroneMessageDispatcher.java
@@ -4,8 +4,10 @@ import ch.helin.messages.converter.JsonBasedMessageConverter;
 import ch.helin.messages.converter.MessageConverter;
 import ch.helin.messages.dto.message.DroneInfoMessage;
 import ch.helin.messages.dto.message.Message;
+import ch.helin.messages.dto.message.missionMessage.ConfirmMissionMessage;
 import com.google.inject.Inject;
 import controllers.DroneInfosController;
+import controllers.MissionController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +19,9 @@ public class DroneMessageDispatcher {
     @Inject
     public DroneInfosController droneInfoController;
 
+    @Inject
+    private MissionController missionController;
+
     public void dispatchMessageToController(UUID droneId, String jsonMessage) {
         MessageConverter messageConverter = new JsonBasedMessageConverter();
         Message message = messageConverter.parseStringToMessage(jsonMessage);
@@ -25,6 +30,11 @@ public class DroneMessageDispatcher {
             case DroneInfo:
                 DroneInfoMessage droneInfoMessage = (DroneInfoMessage) message;
                 droneInfoController.onDroneInfoReceived(droneId, droneInfoMessage);
+                break;
+            case ConfirmMission:
+                ConfirmMissionMessage missionMessage = (ConfirmMissionMessage) message;
+                missionController.onConfirmMissionMessageReceived(droneId, missionMessage);
+
         }
     }
 
