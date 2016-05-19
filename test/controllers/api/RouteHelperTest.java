04d84254a45aab5cb2e911ba3fcadb705069bff8
Styp <mstypinski@gmail.com>
2016-05-19 16:58:17 +0200

    RouteHelperTest completed

--- test/controllers/api/RouteHelperTest.java
+++ test/controllers/api/RouteHelperTest.java
@@ -1,7 +1,9 @@
 package controllers.api;
 
+import ch.helin.messages.dto.Action;
 import ch.helin.messages.dto.way.Position;
 import models.Route;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.LinkedList;
@@ -16,26 +18,7 @@ public class RouteHelperTest {
 
     @Test
     public void shouldConvertPositionListToRoute(){
-
-        List<Position> positions = new LinkedList<>();
-
-        ch.helin.messages.dto.way.Position pos0 = new ch.helin.messages.dto.way.Position();
-        pos0.setHeight(1);
-        pos0.setLon(2.0);
-        pos0.setLat(3.0);
-        positions.add(pos0);
-
-        ch.helin.messages.dto.way.Position pos1 = new ch.helin.messages.dto.way.Position();
-        pos1.setHeight(4);
-        pos1.setLon(5.0);
-        pos1.setLat(6.0);
-        positions.add(pos1);
-
-        ch.helin.messages.dto.way.Position pos2 = new ch.helin.messages.dto.way.Position();
-        pos2.setHeight(7);
-        pos2.setLon(8.0);
-        pos2.setLat(9.0);
-        positions.add(pos2);
+        List<Position> positions = getPositions();
 
         Route route = RouteHelper.positionListToRoute(positions);
 
@@ -46,9 +29,41 @@ public class RouteHelperTest {
          * */
         int truePathLength = (positions.size() * 2) - 1;
 
-
         assertEquals(truePathLength, route.getWayPoints().size());
+    }
 
+    @Test
+    public void shouldContainDropZoneInRoute(){
+
+        List<Position> positions = getPositions();
+        Route route = RouteHelper.positionListToRoute(positions);
+
+        int numOfDropOffZone = (int) route.getWayPoints().stream().filter(x -> x.getAction() == Action.DROP).count();
+
+        assertEquals(1, numOfDropOffZone);
+    }
+
+    private List<Position> getPositions() {
+        List<Position> positions = new LinkedList<>();
+
+        Position pos0 = new Position();
+        pos0.setHeight(1);
+        pos0.setLon(2.0);
+        pos0.setLat(3.0);
+        positions.add(pos0);
+
+        Position pos1 = new Position();
+        pos1.setHeight(4);
+        pos1.setLon(5.0);
+        pos1.setLat(6.0);
+        positions.add(pos1);
+
+        Position pos2 = new Position();
+        pos2.setHeight(7);
+        pos2.setLon(8.0);
+        pos2.setLat(9.0);
+        positions.add(pos2);
+        return positions;
     }
 
 }
