cb6d00674ece9863cec301ad18d1c1d0e58dbf85
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-05-26 15:50:26 +0200

    check for empty missions

--- app/mappers/MissionMapper.java
+++ app/mappers/MissionMapper.java
@@ -1,9 +1,11 @@
 package mappers;
 
+import ch.helin.messages.dto.DroneInfoDto;
 import ch.helin.messages.dto.MissionDto;
 import com.google.inject.Inject;
 import models.Mission;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class MissionMapper {
@@ -23,7 +25,15 @@ public class MissionMapper {
         missionDto.setId(mission.getId());
         missionDto.setRoute(routeMapper.convertToRouteDto(mission.getRoute()));
         missionDto.setOrderProduct(orderProductsMapper.convertToOrderProductDto(mission.getOrderProduct()));
-        missionDto.setDroneInfos(mission.getDroneInfos().stream().map(droneInfoMapper::convertToDroneInfoDto).collect(Collectors.toList()));
+
+        if (mission.getDroneInfos() != null) {
+            List<DroneInfoDto> droneInfosDto = mission.getDroneInfos()
+                .stream()
+                .map(droneInfoMapper::convertToDroneInfoDto)
+                .collect(Collectors.toList());
+
+            missionDto.setDroneInfos(droneInfosDto);
+        }
         if (mission.getState() != null) {
             missionDto.setState(mission.getState().name());
         }
