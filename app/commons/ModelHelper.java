a37ad24397c83eb09ef10fbaeb4d2976a1d703fe
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-05-28 16:21:08 +0200

    added comment

--- app/commons/ModelHelper.java
+++ app/commons/ModelHelper.java
@@ -9,15 +9,21 @@ public class ModelHelper extends BeanUtilsBean {
     @Override
     public void copyProperty(Object dest, String name, Object value)
             throws IllegalAccessException, InvocationTargetException {
-        if (value == null) return;
-        //existing id should not be overwritten when updating attributes
-        if (name.equals("id")) return;
+        if (value == null) {
+            return;
+        }
+
+        // existing id should not be overwritten when updating attributes
+        if (name.equals("id")) {
+            return;
+        }
+
         super.copyProperty(dest, name, value);
     }
 
     /**
      * Be aware, that all setter in destination class must have void as return value.
-     * So no fluent-setter allowed.
+     * So fluent-setter are not allowed.
      */
     public static void updateAttributes(Object destination, Object source) {
         try {
