fca3c29a601101b07f803b50529651bcd2e683ad
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-06-10 17:49:57 +0200

    Show only customers from the current organisations

--- app/dao/CustomerDao.java
+++ app/dao/CustomerDao.java
@@ -1,6 +1,10 @@
 package dao;
 
 import models.Customer;
+import models.Organisation;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class CustomerDao extends AbstractDao<Customer> {
 
@@ -8,4 +12,13 @@ public class CustomerDao extends AbstractDao<Customer> {
         super(Customer.class, "customers");
     }
 
+    public List<Customer> findCustomerByOrganisation(Organisation organisation) {
+        TypedQuery<Customer> query = jpaApi.em().createQuery(
+            "select o.customer from orders o " +
+            " where o.project.organisation.id = :organisation",
+            Customer.class
+        );
+        query.setParameter("organisation", organisation.getId());
+        return query.getResultList();
+    }
 }
